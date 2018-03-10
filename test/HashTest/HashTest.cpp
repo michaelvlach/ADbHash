16a155966d1c9a4804af0c7ca1ba79ca354c7df2
Vlach, Michael <resurrection@centrum.cz>
2018-03-10 23:06:08 +0100

    [Hash] Apply clang-format

--- test/HashTest/HashTest.cpp
+++ test/HashTest/HashTest.cpp
@@ -1,50 +1,46 @@
 #include "HashTest.h"
 
 #include <Hash.h>
-#include <TestExtras.h>
-
 #include <QTest>
+#include <TestExtras.h>
 
 Q_DECLARE_METATYPE(adb::HashTest::Setup)
 QTEST_APPLESS_MAIN(adb::HashTest)
 
 namespace adb
 {
-    void HashTest::initTestCase()
+void HashTest::initTestCase()
+{
+}
+
+void HashTest::init()
+{
+    QFETCH(Setup, setup);
+
+    switch(setup)
     {
-
+    case Setup::None:
+        break;
     }
+}
 
-    void HashTest::init()
-    {
-        QFETCH(Setup, setup);
+void HashTest::constructor()
+{
+    QFAIL("Unimplemented");
+}
 
-        switch(setup)
-        {
-        case Setup::None:
-            break;
-        }
-    }
+void HashTest::constructor_data()
+{
+    QTest::addColumn<Setup>(SETUP);
 
-    void HashTest::constructor()
-    {
-        QFAIL("Unimplemented");
-    }
+    QTest::newRow("Test case description") << Setup::None;
+}
 
-    void HashTest::constructor_data()
-    {
-        QTest::addColumn<Setup>(SETUP);
+void HashTest::cleanup()
+{
+}
 
-        QTest::newRow("Test case description") << Setup::None;
-    }
-
-    void HashTest::cleanup()
-    {
-
-    }
-
-    void HashTest::cleanupTestCase()
-    {
-
-    }
+void HashTest::cleanupTestCase()
+{
+}
 }
