8d1797fec569cc84d1b27d1566398ffceae16a7d
Michael Vlach <vlach.michael@gmail.com>
2018-03-13 00:02:10 +0100

    [Reference] Add class

---
+++ src/Reference.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cstdint>
+#include <utility>
+
+namespace adb
+{
+template<typename T, typename Data>
+class Reference
+{
+public:
+    Reference(Data &data, int64_t index);
+
+    operator T() const;
+    Reference &operator=(T value);
+
+private:
+    Data &mData;
+    int64_t mIndex = -1;
+};
+
+template<typename T, typename Data>
+Reference<T, Data>::Reference(Data &data, int64_t index) :
+    mData(data),
+    mIndex(index)
+{
+}
+
+template<typename T, typename Data>
+Reference<T, Data> &Reference<T, Data>::operator=(T value)
+{
+    mData.setData(mIndex, std::move(value));
+    return *this;
+}
+
+template<typename T, typename Data>
+Reference<T, Data>::operator T() const
+{
+    return mData.value(mIndex);
+}
+}
