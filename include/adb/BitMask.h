2bfa2f1731a3b2dcfe9694e405295ade0982b9ca
Michael Vlach <vlach.michael@gmail.com>
2018-03-29 22:13:24 +0200

    [BitMask] Move internal method from iterator to the class

--- include/adb/BitMask.h
+++ include/adb/BitMask.h
@@ -18,6 +18,10 @@ public:
     bool none() const;
 
 private:
+    friend class BitMask<T>::const_iterator;
+
+    int findNext(int index) const;
+
     const std::bitset<sizeof(T) * CHAR_BIT> mData;
 };
 
@@ -31,7 +35,7 @@ public:
     using difference_type = ptrdiff_t;
     using iterator_category = std::forward_iterator_tag;
 
-    const_iterator(int index, const std::bitset<sizeof(T) * CHAR_BIT> &data);
+    const_iterator(int index, const BitMask<T> &mask);
 
     const_iterator operator++();
     const_iterator operator++(int);
@@ -40,9 +44,7 @@ public:
     bool operator!=(const_iterator other) const;
 
 private:
-    value_type findNext(value_type index);
-
-    const std::bitset<sizeof(T) * CHAR_BIT> &mData;
+    const BitMask<T> &mMask;
     value_type mIndex = static_cast<int>(sizeof(T) * CHAR_BIT);
 };
 
@@ -55,13 +57,13 @@ BitMask<T>::BitMask(T data) :
 template<typename T>
 auto BitMask<T>::begin() const -> const_iterator
 {
-    return const_iterator(0, mData);
+    return const_iterator(findNext(-1), *this);
 }
 
 template<typename T>
 auto BitMask<T>::end() const -> const_iterator
 {
-    return const_iterator(static_cast<int>(sizeof(T) * CHAR_BIT), mData);
+    return const_iterator(static_cast<int>(sizeof(T) * CHAR_BIT), *this);
 }
 
 template<typename T>
@@ -71,16 +73,16 @@ bool BitMask<T>::none() const
 }
 
 template<typename T>
-BitMask<T>::const_iterator::const_iterator(int index, const std::bitset<sizeof(T) * CHAR_BIT> &data) :
-    mData(data),
-    mIndex(findNext(index - 1))
+BitMask<T>::const_iterator::const_iterator(int index, const BitMask<T> &mask) :
+    mIndex(index),
+    mMask(mask)
 {
 }
 
 template<typename T>
 auto BitMask<T>::const_iterator::operator++() -> const_iterator
 {
-    mIndex = findNext(mIndex);
+    mIndex = mMask.findNext(mIndex);
     return *this;
 }
 
@@ -101,7 +103,7 @@ auto BitMask<T>::const_iterator::operator*() const -> value_type
 template<typename T>
 bool BitMask<T>::const_iterator::operator==(const_iterator other) const
 {
-    return mIndex == other.mIndex && mData == other.mData;
+    return mIndex == other.mIndex && mMask.mData == other.mMask.mData;
 }
 
 template<typename T>
@@ -111,7 +113,7 @@ bool BitMask<T>::const_iterator::operator!=(const_iterator other) const
 }
 
 template<typename T>
-auto BitMask<T>::const_iterator::findNext(value_type index) -> value_type
+int BitMask<T>::findNext(int index) const
 {
     while(++index < mData.size() && !mData[index])
         ;
